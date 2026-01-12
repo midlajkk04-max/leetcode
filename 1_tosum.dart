c44359a4818abe6ef405cbed9b43730b66ec3764
midlajkk04-max <midlajkk04@gmail.com>
2026-01-12 15:54:03 +0530

    to sum

---
+++ 1_tosum.dart
@@ -0,0 +1,22 @@
+class Solution {
+  List<int> twoSum(List<int> nums, int target) {
+    for (int i = 0; i < nums.length; i++) {
+      for (int j = i + 1; j < nums.length; j++) {
+        if (nums[i] + nums[j] == target) {
+          return [i, j];
+        }
+      }
+    }
+    return [];
+  }
+}
+
+void main() {
+  Solution solution = Solution();
+
+  List<int> nums = [2, 7, 11, 15];
+  int target = 9;
+
+  List<int> result = solution.twoSum(nums, target);
+  print(result); 
+}
